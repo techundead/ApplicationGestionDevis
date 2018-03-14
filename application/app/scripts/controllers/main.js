601e643615da62d8e9735547b6af3e029710e983
Sylvain D <duclos_sylvain@yahoo.com>
2018-03-14 22:02:46 +0100

    [MADERA] Création du squelette AngularJS du projet

---
+++ application/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name maderaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the maderaApp
+ */
+angular.module('maderaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
